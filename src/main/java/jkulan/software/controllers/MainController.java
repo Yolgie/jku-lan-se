c0ac0bc84233cbf729b06000ca8054780a086e19
Astavinu <astavinu@gmx.at>
2016-01-18 19:37:33 +0100

    initial steam-condenser-java implementation

--- src/main/java/jkulan/software/controllers/MainController.java
+++ src/main/java/jkulan/software/controllers/MainController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.koraktor.steamcondenser.community.SteamId;
+import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
+
 /**
  * Created by tth on 1/18/16.
  */
@@ -13,7 +16,15 @@ public class MainController {
     @RequestMapping("/")
     @ResponseBody
     public String index() {
-        return "Proudly handcrafted by TTH";
+    	String id = "fuero";
+    	SteamId fuero;
+		try {
+			fuero = SteamId.create(id, true);
+	        return fuero.getNickname();
+		} catch (SteamCondenserException e) {
+			e.printStackTrace();
+		}
+		return "Steam does not know "+id;
     }
 
 }

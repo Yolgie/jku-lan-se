f27e7c310c53fbecd9049666d486b8d65cf67bd3
Matthias Holzinger <yolgie@cnoize.at>
2016-02-20 19:40:41 +0100

    3-refactor-reformatting

--- src/main/java/jkulan/Application.java
+++ src/main/java/jkulan/Application.java
@@ -3,9 +3,6 @@ package jkulan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by tth on 1/18/16.
- */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/jkulan/software/controllers/MainController.java
+++ src/main/java/jkulan/software/controllers/MainController.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by tth on 1/18/16.
- */
 @Controller
 public class MainController {
 

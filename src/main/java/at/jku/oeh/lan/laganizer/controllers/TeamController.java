ccbda876341247c20208c735b372c7c6a8b4cb42
Astavinu <astavinu@gmx.at>
2016-02-21 04:15:39 +0100

    12-refactor-imports fixed

--- src/main/java/at/jku/oeh/lan/laganizer/controllers/TeamController.java
+++ src/main/java/at/jku/oeh/lan/laganizer/controllers/TeamController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.jku.oeh.lan.laganizer.dto.RESTDataWrapperDTO;
 import at.jku.oeh.lan.laganizer.model.Team;
 import at.jku.oeh.lan.laganizer.model.TeamDAO;
-import jkulan.software.dto.RESTDataWrapperDTO;
-import jkulan.software.model.TournamentDAO;
-import jkulan.software.model.User;
-import jkulan.software.model.UserDAO;
+import at.jku.oeh.lan.laganizer.model.TournamentDAO;
+import at.jku.oeh.lan.laganizer.model.User;
+import at.jku.oeh.lan.laganizer.model.UserDAO;
 
 @RestController
 @RequestMapping("/teams/")
--- src/main/java/at/jku/oeh/lan/laganizer/model/Team.java
+++ src/main/java/at/jku/oeh/lan/laganizer/model/Team.java
@@ -1,14 +1,16 @@
 package at.jku.oeh.lan.laganizer.model;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
-public class Team {
+public class Team implements Serializable{
 	@Id
 	@GeneratedValue
 	private long id;
@@ -17,5 +19,37 @@ public class Team {
 	private List<User> players;
 	
 	@NotNull
+	@ManyToOne
 	private Tournament tournament;
+	
+	@NotNull
+	private String name;
+
+	public List<User> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<User> players) {
+		this.players = players;
+	}
+
+	public Tournament getTournament() {
+		return tournament;
+	}
+
+	public void setTournament(Tournament tournament) {
+		this.tournament = tournament;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
 }
